f6fa6e7860cac43a6af3046f430f6612d180fed3
Commander Zoomie <commanderzoomie@gmail.com>
2026-02-11 22:11:33 -0500

    Add metadata including author, copyright, and version

--- client.py
+++ client.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__author__ = "Anthony Nicholas Carmen"
+__copyright__ = "Copyright 2026, Anthony Nicholas Carmen"
+__license__ = "Apache-2.0"
+__version__ = "1.0.1"
+
 import socket
 import threading
 import sys
